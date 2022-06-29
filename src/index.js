38abb0825a7bac4a941011b9313f8431a09e84ba
Matheus Neves <matheusneves1989@gmail.com>
2022-06-29 12:02:49 -0300

    feat(account): create post route

--- src/index.js
+++ src/index.js
@@ -1,6 +1,41 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid')
 const app = express();
 
 app.use(express.json())
+
+const mockedCustomers = [];
+const accountResource = '/account'
+
+/**
+ * cpf - string
+ * name - string
+ * id - uuid
+ * statement - []
+ */
+app.post(accountResource, (req, res) => {
+  try {
+    const { cpf, name } = req.body;
+
+    const customerAlreadyExists = mockedCustomers.some(customer => customer.cpf === cpf)
+
+    if(customerAlreadyExists) throw new Error('Customer already exists');
+
+    const newCustomer = {
+      id: uuidv4(),
+      cpf,
+      name,
+      statement: []
+    }
+
+    mockedCustomers.push(newCustomer)
+
+    res.status(201).json(newCustomer)
+  } catch (error) {
+    res.status(409).json({
+      message: error.message
+    })
+  }
+})
 
 app.listen(3333, () => console.log('server running...'))
